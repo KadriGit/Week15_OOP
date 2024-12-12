dd9b5097a58b48e21d151a8300c6bfb9db9fd775
KadriGit <kadri.viira@tktk.ee>
2024-12-12 23:20:06 +0200

    final code

--- Week15_OOP/Program.cs
+++ Week15_OOP/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] data = GetDataFromFile();
+List<Movie> list = new List<Movie>();
+
+foreach (string line in data)
+{
+    string[] temparray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(temparray[0], temparray[2], temparray[1]);
+    list.Add(newMovie);
+}
+
+foreach (Movie movie in list)
+{
+    Console.WriteLine($"One of my favorite movie {movie.title}  was released in {movie.year}.");
+}
+
+static string[] GetDataFromFile()
+{
+    string filePath = @"C:\Users\Lenovo\Desktop\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+class Movie
+{
+    public string title;
+    public string rating;
+    public string year;
+
+    public Movie(string _title, string _rating, string _year)
+    {
+        title = _title;
+        rating = _rating;
+        year = _year;
+    }
+}
